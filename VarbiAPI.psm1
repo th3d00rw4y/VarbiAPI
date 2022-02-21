1a1ad0e88857670620807666d13d55e41110e00f
Simon Mellergård <SIMMEL01@varnamo.se>
2022-02-21 17:14:54 +0100

    Rewrote most of psm1 file

--- VarbiAPI.psm1
+++ VarbiAPI.psm1
@@ -1,37 +1,40 @@
 $Script:ModuleRoot = $PSScriptRoot
-$Script:VarbiSettingsFilePath = "$env:TEMP\VarbiAPISettings-$($env:USERNAME)_$($env:COMPUTERNAME).csv"
+$Script:VarbiCheckSettingsFilePath = "$env:TEMP\VarbiCheck-$($env:USERNAME)_$($env:COMPUTERNAME).checkfile"
+# $Script:VarbiSettingsFilePath = "$env:TEMP\VarbiAPISettings-$($env:USERNAME)_$($env:COMPUTERNAME).csv"
 
-$Private = @(Get-ChildItem -Path $ModuleRoot\Private\*.ps1 -ErrorAction SilentlyContinue)
-$Public  = @(Get-ChildItem -Path $ModuleRoot\Public\*.ps1 -ErrorAction SilentlyContinue)
-
-foreach ($Import in @($Private + $Public)) {
-    try {
-        . $Import.FullName
-    }
-    catch {
-        Write-Error -Message "Failed to import function $($Import.FullName): $_"
-    }
+if (-not (Test-Path $VarbiCheckSettingsFilePath)) {
+    Write-Warning -Message "No settings file found. Please configure the module by running Initialize-SettingsFile provided with your information."
 }
-
-if (-not (Test-Path $VarbiSettingsFilePath)) {
-
-    Clear-Host
-    Write-Output "Select credential file for encrypted API key"
-    $APIKeyPath= Get-FilePath
-
-    $Server = Read-Host -Prompt "Enter server URI (e.g: ""https://api.varbi.com/v1"")"
-
-    $Table = [PSCustomObject]@{
-        APIKeyPath   = $APIKeyPath
-        Server       = $Server
-    }
-
-    $Table | ConvertTo-Csv -NoTypeInformation | Set-Content -Path $VarbiSettingsFilePath -Encoding UTF8
-
-    $Script:Settings = Import-Csv -Path $VarbiSettingsFilePath
+elseif (-not (Test-Path -Path $(Get-Content -Path $DFRCheckSettingsFilePath))) {
+    Write-Warning -Message "No settings file found. Please configure the module by running Initialize-SettingsFile provided with your information."
 }
 else {
-    $Script:Settings = Import-Csv -Path $VarbiSettingsFilePath
+    $Script:Settings     = Import-Csv -Path (Get-Content -Path $VarbiCheckSettingsFilePath)
+    $Script:ADProperties = @($Settings.PSObject.Properties | Where-Object {($_.Name -notlike '*Path') -and ($_.Name -ne 'Server')} | Select-Object -ExpandProperty Value)
+    $SettingsFileExists  = $true
+
+    $Private = @(Get-ChildItem -Path $ModuleRoot\Private\*.ps1 -ErrorAction SilentlyContinue)
+    $Public  = @(Get-ChildItem -Path $ModuleRoot\Public\*.ps1 -ErrorAction SilentlyContinue)
+    $Nested  = @(Get-ChildItem -Path $ModuleRoot\Resources\SecretClixml\Public\*.ps1 -ErrorAction SilentlyContinue)
+
+    foreach ($Import in @($Private + $Public + $Nested)) {
+        try {
+            . $Import.FullName
+        }
+        catch {
+            Write-Error -Message "Failed to import function $($Import.FullName): $_"
+        }
+    }
+
+    Export-ModuleMember -Function $Public.Basename
+    Export-ModuleMember -Function $Nested.Basename
 }
 
-Export-ModuleMember -Function $Public.Basename -Variable Settings
+
+
+if ($SettingsFileExists -eq $true) {
+    Export-ModuleMember -Variable Settings, ADProperties
+}
+else {
+    Export-ModuleMember -Variable VarbiCheckSettingsFilePath
+}
